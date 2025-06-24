e9f6833aeef4a40c20ad1fd18d6840fcbf91a868
AoSankaku <58819229+AoSankaku@users.noreply.github.com>
2025-06-24 22:45:25 +0900

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import './App.css'
+import styled from 'styled-components'
+import { Tabs, Tab, Button } from '@mui/material'
+
+
+function App() {
+
+  return (
+    <>
+      <Title>日本語パスフレーズジェネレーター</Title>
+      <div>
+        <p>日本語でパスフレーズ（パスワードの代わりになるもの）を作れます。</p>
+      </div>
+      <PassPhrase>
+        nihon.gengo.pasuwa-do.kawarini.naruyo
+      </PassPhrase>
+      <Button>生成</Button>
+    </>
+  )
+}
+
+const Title = styled.h1`
+`
+
+const PassPhrase = styled.div`
+  font-size: 3rem;
+  font-weight: bold;
+  padding: 1rem 0;
+`
+
+export default App
