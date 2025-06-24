0f0f2cb8918274566c1d46962bedf4643ee6a7a4
AoSankaku <58819229+AoSankaku@users.noreply.github.com>
2025-06-25 01:56:26 +0900

    Refactor passPhrase state to use a structured type and update generation logic for kanji passphrase

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,17 @@ import { useState, useEffect } from 'react'
 import Papa from 'papaparse';
 import wordlist_csv from "./assets/wordlist.csv?raw";
 
+type PassPhrase = {
+  passPhrase: string;
+  kanjiPassPhrase: string;
+}
 
 function App() {
   const [wordlist, setWordlist] = useState<any>()
-  const [passPhrase, setPassPhrase] = useState("nihon.gengo.pasuwa-do.kawarini.naruyo")
+  const [passPhrase, setPassPhrase] = useState<PassPhrase>({
+    passPhrase: "nihon.gengo.pasuwa-do.kawarini.naruyo",
+    kanjiPassPhrase: "日本語パスワード代わりになるよ"
+  })
   const [separator, setSeparator] = useState(".")
 
   useEffect(() => {
@@ -20,15 +27,19 @@ function App() {
   }, [wordlist])
 
   const generatePassPhrase = () => {
-    
-   /*ランダムパスワード生成*/ 
-    const union_pass = []
-    for(let i = 0; i < 4; i++){
+
+    /*ランダムパスワード生成*/
+    const union_pass: [string[], string[]] = [[], []]
+    for (let i = 0; i < 4; i++) {
       const rand_index = Math.floor(Math.random() * wordlist.data.length)
-      const pass_parts = wordlist.data[rand_index][1]
-      union_pass.push(pass_parts)
+      const pass_parts = wordlist.data[rand_index]
+      union_pass[0].push(pass_parts[0])
+      union_pass[1].push(pass_parts[1])
     }
-    setPassPhrase([union_pass].join(separator))
+    setPassPhrase({
+      passPhrase: [union_pass[1]].join(separator),
+      kanjiPassPhrase: [union_pass[0]].join(separator)
+    })
   }
 
   return (
@@ -38,8 +49,11 @@ function App() {
         <p>日本語でパスフレーズ（パスワードの代わりになるもの）を作れます。</p>
       </div>
       <PassPhrase>
-        {passPhrase}
+        {passPhrase.passPhrase}
       </PassPhrase>
+      <KanjiPassPhrase>
+        {passPhrase.kanjiPassPhrase}
+      </KanjiPassPhrase>
       <Button onClick={() => generatePassPhrase()}>生成</Button>
     </>
   )
@@ -52,6 +66,13 @@ const PassPhrase = styled.div`
   font-size: 3rem;
   font-weight: bold;
   padding: 1rem 0;
+`
+
+const KanjiPassPhrase = styled.div`
+  font-size: 2rem;
+  font-weight: bold;
+  padding:  0 0 1rem 0;
+  color: #888;
 `
 
 export default App
