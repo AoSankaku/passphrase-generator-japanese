2ed7a0c21962e32b30d53fe35234958190905681
sushimero <gabu0626@iclod.com>
2025-06-29 22:49:52 +0900

    ていせい

--- src/assets/WordListSorter.py
+++ src/assets/WordListSorter.py
@@ -8,6 +8,6 @@ def WordListSorter():
   
   wordlist = wordlist[wordlist[1].str.len() <= 5]
   
-  wordlist.to_csv("new_wordlist.csv", index = False, encoding = "utf-8")
+  wordlist.to_csv("wordlist.csv", index = False, encoding = "utf-8")
   
 WordListSorter()
